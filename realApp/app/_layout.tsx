97cecd1e1b12c0ce73e386f998738b185a277766
DanielleBoswell <danituongvy@gmail.com>
2025-09-23 17:47:55 -0500

    Add Settings page, Add Tamagui

--- realApp/app/_layout.tsx
+++ realApp/app/_layout.tsx
@@ -1,9 +1,22 @@
 // app/_layout.tsx
+import { TamaguiProvider, View, createTamagui } from '@tamagui/core'
+import { defaultConfig } from '@tamagui/config/v4'
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useColorScheme } from 'react-native';
+import React from 'react';
+
+// you usually export this from a tamagui.config.ts file
+const config = createTamagui(defaultConfig)
+
+type Conf = typeof config
+
+// make imports typed
+declare module '@tamagui/core' {
+  interface TamaguiCustomConfig extends Conf {}
+}
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -16,14 +29,15 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        {/* The tabs group */}
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        {/* Optional: 404 fallback */}
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <TamaguiProvider config={config}>
+        <Stack>
+          {/* The tabs group */}
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          {/* Optional: 404 fallback */}
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
+    </TamaguiProvider>
+    
   );
 }
